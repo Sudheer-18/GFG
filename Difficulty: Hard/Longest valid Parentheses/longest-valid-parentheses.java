0ee46ebb41f8bcaf1887e4c0ba8921f68688470b
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-02-22 08:24:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.java
@@ -1,40 +1,40 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
+        while (t-- > 0) {
             String S = in.readLine();
-            
+
             Solution ob = new Solution();
             System.out.println(ob.maxLength(S));
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
+
+
 class Solution {
-    static int maxLength(String S) {
-        Stack<Integer> st = new Stack<>();
-        st.push(-1); 
+    static int maxLength(String s) {
+        // code here
+        Stack<Integer> st  = new Stack<>();
+        st.push(-1);
         int maxLen = 0;
-        for (int i = 0; i < S.length(); i++) {
-            char ch = S.charAt(i);
-            if (ch == '(') {
-                st.push(i); 
-            } else {
-                st.pop(); 
-                if (st.isEmpty()) {
-                    st.push(i); 
-                } else {
-                    maxLen = Math.max(maxLen, i - st.peek()); 
-                }
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(') st.push(i);
+            else {
+                st.pop();
+                if(st.isEmpty()) st.push(i);
+                maxLen = Math.max(maxLen,i - st.peek());
             }
         }
         
