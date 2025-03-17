4af11e1d3f162215bc92b687a6e2056f8e3210f2
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-03-17 21:41:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product of Two Numbers/product-of-two-numbers.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    static int product(int x, int y) {
+        // code here
+        return x* y;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String[] inp = read.readLine().split(" ");
+            int x = Integer.parseInt(inp[0]);
+            int y = Integer.parseInt(inp[1]);
+            Solution ob = new Solution();
+            System.out.println(ob.product(x, y));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
